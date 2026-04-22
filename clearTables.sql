9b6faa665bacd45a6adb2c094acdda7bf8890654
Jenn <znzhenn@gmail.com>
2026-04-22 09:43:00 -0400

    clearing tables, reprinting

---
+++ clearTables.sql
@@ -0,0 +1,20 @@
+-- set foreign_key_checks = 0;
+
+truncate table pc;
+truncate table printer;
+truncate table printer3d;
+
+truncate table room260;
+truncate table room259;
+
+truncate table users;
+
+-- set foreign_keys_checks = 1;
+
+-- print test data
+select * from users;
+select * from room260;
+select * from room259;
+select * from pc;
+select * from printer;
+select * from printer3d;
