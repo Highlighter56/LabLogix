df6c62483c4dc760aea8abe80c14475c984b7d1e
Jenn <znzhenn@gmail.com>
2026-04-28 11:17:18 -0400

    databaseInstructions logged

--- database files/testFile.sql
+++ database files/testFile.sql
@@ -1,11 +1,17 @@
 -- show databases;
 use labLogix;
 show tables;
-describe users;
-describe room260;
-describe room259;
-describe pc;
-describe printer;
-describe printer3d;
+-- describe users;
+-- describe room260;
+-- describe room259;
+-- describe pc;
+-- describe printer;
+-- describe printer3d;
 
-
+-- show the data in each table
+select * from users;
+select * from room260;
+select * from room259;
+select * from pc;
+select * from printer;
+select * from printer3d;
